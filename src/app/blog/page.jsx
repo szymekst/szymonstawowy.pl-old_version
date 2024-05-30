a209a11a7126d1a6f90f4a33166b47459f1b411f
Szymek <Denek222@gmail.com>
2024-05-30 12:18:05 +0200

    [ADD] Fully working pagination

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 //Import utils and components
 import getPostMetadata from "@/utils/getPostMetadata";
 import TitleSection from "@components/TitleSection";
+import PaginationControls from "@components/PaginationControls";
 
 const createDate = (stringDate) => {
     const days = [
@@ -51,9 +52,21 @@ const createDate = (stringDate) => {
     return stringToReturn;
 };
 
-const page = () => {
+const page = ({ searchParams }) => {
     const postMetadata = getPostMetadata("posts");
-    const numPages = postMetadata.length;
+
+    const currentPage = searchParams["currentPage"] ?? "1";
+    const postsPerPage = searchParams["postsPerPage"] ?? "3";
+
+    const skipPosts = (Number(currentPage) - 1) * Number(postsPerPage);
+    const limitPosts = skipPosts + Number(postsPerPage);
+
+    const allPosts = postMetadata.slice(skipPosts, limitPosts);
+
+    const numOfPages = Math.ceil(
+        Number(postMetadata.length) / Number(postsPerPage)
+    );
+
     return (
         <>
             <TitleSection title="The Blog" />
@@ -63,7 +76,7 @@ const page = () => {
                         All blog posts
                     </h2>
                     <div className="flex flex-col gap-[16px] md:grid md:grid-cols-2 md:gap-x-[16px] md:gap-y-[24px] lg:grid-cols-3 lg:gap-x-[32px] lg:gap-y-[48px] ">
-                        {postMetadata.map((post, index) => {
+                        {allPosts.map((post, index) => {
                             return (
                                 <Link
                                     key={index}
@@ -95,103 +108,13 @@ const page = () => {
                             );
                         })}
                     </div>
-                    {/* {numPages > 1 ? (
-                        <div className="pt-[30px] pb-[50px] flex flex-col items-center gap-[20px] text-sm font-medium dark:text-white md:flex-row md:justify-between md:pt-[50px] md:pb-[60px] lg:py-[30px]">
-                            <Link
-                                to={prevPage}
-                                rel="prev"
-                                className={
-                                    prevIsFirst ? "pointer-events-none" : ""
-                                }
-                            >
-                                ← Poprzednia
-                            </Link>
-                            <div className="flex justify-center gap-[2px]">
-                                {Array.from({ length: numPages }, (_, i) =>
-                                    numPages >= 8 ? (
-                                        <>
-                                            {i < 3 ? (
-                                                <Link
-                                                    key={`pagination-number${
-                                                        i + 1
-                                                    }`}
-                                                    to={`${
-                                                        i === 0
-                                                            ? "/blog"
-                                                            : "/blog/" + (i + 1)
-                                                    }`}
-                                                    className="w-[40px] h-[40px] rounded-[8px] flex justify-center items-center"
-                                                    activeClassName="!bg-purple-200 text-purple-100 dark:!bg-white dark:text-black-100"
-                                                >
-                                                    {i + 1}
-                                                </Link>
-                                            ) : (
-                                                ""
-                                            )}
-                                            {i === 4 ? (
-                                                <div className="w-[40px] h-[40px] rounded-[8px] flex justify-center items-center">
-                                                    ...
-                                                </div>
-                                            ) : (
-                                                ""
-                                            )}
-
-                                            {i >= numPages - 3 &&
-                                            i <= numPages ? (
-                                                <>
-                                                    <Link
-                                                        key={`pagination-number${
-                                                            i + 1
-                                                        }`}
-                                                        to={`${
-                                                            i === 0
-                                                                ? "/blog"
-                                                                : "/blog/" +
-                                                                  (i + 1)
-                                                        }`}
-                                                        className="w-[40px] h-[40px] rounded-[8px] flex justify-center items-center"
-                                                        activeClassName="!bg-purple-200 text-purple-100 dark:!bg-white dark:text-black-100"
-                                                    >
-                                                        {i + 1}
-                                                    </Link>
-                                                </>
-                                            ) : (
-                                                ""
-                                            )}
-                                        </>
-                                    ) : (
-                                        <>
-                                            <Link
-                                                key={`pagination-number${
-                                                    i + 1
-                                                }`}
-                                                to={`${
-                                                    i === 0
-                                                        ? "/blog"
-                                                        : "/blog/" + (i + 1)
-                                                }`}
-                                                className="w-[40px] h-[40px] rounded-[8px] flex justify-center items-center"
-                                                activeClassName="!bg-purple-200 text-purple-100 dark:!bg-white dark:text-black-100"
-                                            >
-                                                {i + 1}
-                                            </Link>
-                                        </>
-                                    )
-                                )}
-                            </div>
-                            <Link
-                                to={nextPage}
-                                rel="next"
-                                className={
-                                    nextIsLast ? "pointer-events-none" : ""
-                                }
-                            >
-                                Nastepna →
-                            </Link>
-                        </div>
-                    ) : (
-                        <></>
-                    )} */}
+                    <PaginationControls
+                        currentPageProp={currentPage}
+                        postsPerPageProp={postsPerPage}
+                        numOfPages={numOfPages}
+                        hasPrevPage={skipPosts > 0}
+                        hasNextPage={limitPosts < postMetadata.length}
+                    />
                 </div>
             </div>
         </>
---
+++ src/components/PaginationControls.jsx
@@ -0,0 +1,129 @@
+"use client";
+
+import React from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+
+const PaginationControls = ({
+    currentPageProp,
+    postsPerPageProp,
+    numOfPages,
+    hasPrevPage,
+    hasNextPage,
+}) => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    const currentPage = searchParams.get("currentPage") ?? currentPageProp;
+    const postsPerPage = searchParams.get("postPerPage") ?? postsPerPageProp;
+
+    const prevPagePath = `/blog/?currentPage=${
+        Number(currentPage) - 1
+    }&postsPerPage=${postsPerPage}`;
+
+    const nextPagePath = `/blog/?currentPage=${
+        Number(currentPage) + 1
+    }&postsPerPage=${postsPerPage}`;
+
+    return (
+        <div>
+            {numOfPages > 1 ? (
+                <div className="pt-[30px] pb-[50px] flex flex-col items-center gap-[20px] text-sm font-medium dark:text-white md:flex-row md:justify-between md:pt-[50px] md:pb-[60px] lg:py-[30px]">
+                    <button
+                        disabled={!hasPrevPage}
+                        onClick={() => {
+                            router.push(prevPagePath);
+                        }}
+                    >
+                        ← Poprzednia
+                    </button>
+                    <div className="flex justify-center gap-[2px]">
+                        {Array.from({ length: numOfPages }, (_, i) =>
+                            numOfPages >= 8 ? (
+                                <React.Fragment key={i}>
+                                    {i < 3 ? (
+                                        <button
+                                            className={`${
+                                                currentPage == i + 1 &&
+                                                "pagination_button__active"
+                                            } pagination_button`}
+                                            onClick={() => {
+                                                router.push(
+                                                    `/blog/?currentPage=${
+                                                        i + 1
+                                                    }&postsPerPage=${postsPerPage}`
+                                                );
+                                            }}
+                                        >
+                                            {i + 1}
+                                        </button>
+                                    ) : (
+                                        ""
+                                    )}
+                                    {i === 4 ? (
+                                        <div className="pagination_button">
+                                            ...
+                                        </div>
+                                    ) : (
+                                        ""
+                                    )}
+
+                                    {i >= numOfPages - 3 && i <= numOfPages ? (
+                                        <>
+                                            <button
+                                                className={`${
+                                                    currentPage == i + 1 &&
+                                                    "pagination_button__active"
+                                                } pagination_button`}
+                                                onClick={() => {
+                                                    router.push(
+                                                        `/blog/?currentPage=${
+                                                            i + 1
+                                                        }&postsPerPage=${postsPerPage}`
+                                                    );
+                                                }}
+                                            >
+                                                {i + 1}
+                                            </button>
+                                        </>
+                                    ) : (
+                                        ""
+                                    )}
+                                </React.Fragment>
+                            ) : (
+                                <React.Fragment key={i}>
+                                    <button
+                                        className={`${
+                                            currentPage == i + 1 &&
+                                            "pagination_button__active"
+                                        } pagination_button`}
+                                        onClick={() => {
+                                            router.push(
+                                                `/blog/?currentPage=${
+                                                    i + 1
+                                                }&postsPerPage=${postsPerPage}`
+                                            );
+                                        }}
+                                    >
+                                        {i + 1}
+                                    </button>
+                                </React.Fragment>
+                            )
+                        )}
+                    </div>
+                    <button
+                        disabled={!hasNextPage}
+                        onClick={() => {
+                            router.push(nextPagePath);
+                        }}
+                    >
+                        Nastepna →
+                    </button>
+                </div>
+            ) : (
+                <></>
+            )}
+        </div>
+    );
+};
+
+export default PaginationControls;
